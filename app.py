f1c3f5a3908653bbf9118e09e756f820a214d78b
mingkaid <dengmingkai@gmail.com>
2018-01-16 19:49:05 +0000

    Added personal page with name, picture and favorite color. Expect 404

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/md3422', methods=['GET'])
+def md3422():
+    return render_template('md3422.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8888)
